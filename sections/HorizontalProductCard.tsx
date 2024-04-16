6161f187a4c6cfa9051a0be6c826a163993f5803
Mateus Carvalho <mateusprofile4@hotmail.com>
2024-04-16 02:46:19 -0300

    feat: decoCamp task 5

--- sections/HorizontalProductCard.tsx
+++ sections/HorizontalProductCard.tsx
@@ -1,5 +1,9 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Button from "deco-sites/projeto-camp/components/ui/Button.tsx";
+import { AppContext } from "deco-sites/projeto-camp/apps/site.ts";
+import Image from "apps/website/components/Image.tsx";
+import Loading from "deco-sites/projeto-camp/components/daisy/Loading.tsx";
+import { LoadingFallback } from "deco-sites/projeto-camp/sections/Product/ProductDetails.tsx";
 
 export interface ProductDetail {
   image: ImageWidget;
@@ -12,6 +16,62 @@ export interface Props {
   items: ProductDetail[];
 }
 
+export function ErrorFallback({ error }: { error?: Error }) {
+  return (
+    <>
+      <div class="bg-gray-200 flex flex-col md:flex-row space-y-4 mb-2 rounded p-4">
+        <div class="rounded">
+          <Image
+            src="https://s5.static.brasilescola.uol.com.br/be/2023/08/cultura-nordeste-quadrilha.jpg"
+            alt="Cultura do Nordeste: elementos, origens"
+            class="w-full md:w-36 rounded"
+            width={144}
+          />
+        </div>
+        <div class="flex-1 flex justify-center items-center">
+          <h3>Cultura do Nordeste: elementos, origens</h3>
+          <p>
+            "A cultura do Nordeste do Brasil é o conjunto de tradições e de
+            manifestações artísticas e culturais que exprimem a identidade da
+            sua população" Veja mais sobre "Cultura do Nordeste" em:
+            https://brasilescola.uol.com.br/brasil/aspectos-culturais-regiao-nordeste.htm
+          </p>
+        </div>
+        <div class="flex flex-col justify-center items-center space-y-2">
+          <a href="/cultura">Para saber mais</a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export const loader = (props: Props, req: Request, ctx: AppContext) => {
+  if (!props.items || props.items?.length === 0) {
+    return (
+      <>
+        <div class="bg-gray-200 flex flex-col md:flex-row space-y-4 mb-2 rounded p-4">
+          <div class="rounded">
+            <Image
+              src=""
+              alt="Cultura do Nordeste: elementos, origens"
+              class="w-full md:w-36 rounded bg-slate-500"
+              width={144}
+            />
+          </div>
+          <div class="flex-1 flex justify-center items-center">
+            <LoadingFallback />
+          </div>
+          <div class="flex flex-col justify-center items-center space-y-2">
+            <LoadingFallback />
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return props;
+};
+
 export default function HorizontalProductCard(prop: Props) {
   return (
     <>
@@ -20,7 +80,12 @@ export default function HorizontalProductCard(prop: Props) {
           prop.items.map((item) => (
             <div class="bg-gray-200 flex flex-col md:flex-row space-y-4 mb-2 rounded p-4">
               <div class="rounded">
-                <img src={item.image} alt="" class="w-full md:w-36 rounded" />
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  class="w-full md:w-36 rounded"
+                  width={144}
+                />
               </div>
               <div class="flex-1 flex justify-center items-center">
                 <h3>{item.title}</h3>
